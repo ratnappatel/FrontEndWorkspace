579b3959ea87f18bbfe6b949ded1f16c8e2e8470
ratnappatel <ratnappatel@gmail.com>
2023-01-24 11:42:44 +0530

    updated

--- HCL_ReactJS/EmployeeManagementSystem/emp-client/src/Components/AddEmployee.js
+++ HCL_ReactJS/EmployeeManagementSystem/emp-client/src/Components/AddEmployee.js
@@ -5,7 +5,7 @@ export default class AddEmployee extends Component {
     constructor(props) {
       super(props)
     
-      this.state = {
+      this.state = {        
             "id":0,
             "name":'',
             "desig":'',
@@ -14,7 +14,7 @@ export default class AddEmployee extends Component {
     }
 
     handleChange=(e)=>{
-        let controlName=e.target.name;
+        var controlName=e.target.name;
         let value=e.target.value;
         this.setState({
             controlName :value
@@ -22,8 +22,9 @@ export default class AddEmployee extends Component {
     }
     handleSubmit=(e)=>{
         e.preventDefault();
-        addEmployee(this.state.employee);
-        console.log("Employee Details added.")
+        addEmployee(this.state);
+        
+        console.log("Employee Details added."+this.state)
     }
 
 
@@ -33,22 +34,22 @@ export default class AddEmployee extends Component {
         <form onSubmit={this.handleSubmit}>
             <fieldset>
                 <label>Enter Employee ID</label>
-                <input type="text" name="employee.id" value={this.state.id}
+                <input type="text" name="id" 
                  onChange={this.handleChange}></input>
             </fieldset>
             <fieldset>
                 <label>Enter Employee Name</label>
-                <input type="text" name="employee.name" value={this.state.name}
+                <input type="text" name="name"
                  onChange={this.handleChange}></input>
             </fieldset>
             <fieldset>
                 <label>Enter Employee Designation</label>
-                <input type="text" name="employee.desig" value={this.state.desig}
+                <input type="text" name="desig" 
                  onChange={this.handleChange}></input>
             </fieldset>
             <fieldset>
                 <label>Enter Employee Salary</label>
-                <input type="text" name="employee.salary" value={this.state.salary}
+                <input type="text" name="salary" 
                  onChange={this.handleChange}></input>
             </fieldset>
             <button type="Submit">Add Employee</button>
