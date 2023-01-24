4a49bc602fe615639923d300c425d9f40ca6cc02
ratnappatel <ratnappatel@gmail.com>
2023-01-24 17:01:48 +0530

    Parameter Used

--- HCL_ReactJS/EmployeeManagementSystem/emp-client/src/Components/EditEmployee.js
+++ HCL_ReactJS/EmployeeManagementSystem/emp-client/src/Components/EditEmployee.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
-import {updateEmployeeDetails as update} from '../Services/employee';
-import {withRouter} from 'react-router';
+import {getEmployeeDetails, updateEmployeeDetails as update} from '../Services/employee';
+import withRouter from './withRouter';
 
-export default class EditEmployee extends Component {
+
+ class EditEmployee extends Component {
     constructor(props) {
       super(props)
     
       this.state = {
-         "id":0,
-         "name":'',
-         "desig":'',
-         "salary":0
+        "employee":{
+         "id":Number,
+         "name":String,
+         "desig":String,
+         "salary":Number
+        }
       }
     }
     componentDidMount(){
-      console.log(this.props.match.params);
+      getEmployeeDetails(this.props.params.id).then(res => {
+        const arr = res.data;
+        console.log(arr);
+        this.setState({ employee: arr });
+      });
+      console.log(this.props.params.id);
     }
-    handleSubmit=()=>{
-      update(this.state.id, this.state)
+    handleSubmit=(e)=>{
+      e.preventDefault();
+      update(this.state.id, this.state);
+      
     }
   render() {
     return (
@@ -25,22 +35,22 @@ export default class EditEmployee extends Component {
         <form onSubmit={this.handleSubmit}>
             <fieldset>
                 <label>Enter Employee ID</label>
-                <input type="text" name="id" value={this.state.id}
-                 onChange={e=>{this.setState({id: e.target.value});}}></input>
+                <input type="text" name="id" value={this.state.employee.id} readOnly
+                 ></input>
             </fieldset>
             <fieldset>
                 <label>Enter Employee Name</label>
-                <input type="text" name="name" value={this.state.name}
+                <input type="text" id="name" name="name" defaultValue={this.state.employee.name}
                  onChange={e=>{this.setState({name: e.target.value});}}></input>
             </fieldset>
             <fieldset>
                 <label>Enter Employee Designation</label>
-                <input type="text" name="desig"  value={this.state.desig}
+                <input type="text" id="desig" name="desig"  defaultValue={this.state.employee.desig}
                  onChange={e=>{this.setState({desig: e.target.value});}}></input>
             </fieldset>
             <fieldset>
                 <label>Enter Employee Salary</label>
-                <input type="text" name="salary" value={this.state.salary}
+                <input type="text" id="salary" name="salary" defaultValue={this.state.employee.salary}
                  onChange={e=>{this.setState({salary: e.target.value});}}></input>
             </fieldset>
             <button type="Submit">Update Employee</button>
@@ -50,4 +60,4 @@ export default class EditEmployee extends Component {
   }
 }
 
- EditEmployee=withRouter(EditEmployee);
+export default withRouter(EditEmployee);
