16381f4daacff1beb3842d85db16bfa3b0d6e2f0
ratnappatel <ratnappatel@gmail.com>
2023-01-27 11:11:30 +0530

    conditional rendering

---
+++ HCL_ReactJS/cond-render/src/Components.js/LoginPage.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './LoginPage.css'
+
+export default class LoginPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          isLoggedIn: true
+        };
+      }
+    handleClick=()=>{
+        this.setState({isLoggedIn:false});
+    }
+      render() {
+        let {isLoggedIn} = this.state;
+
+        const renderAuthButton = () => {
+          if (isLoggedIn) {
+            return <button>Logout</button>;
+          } else {
+            return <button>Login</button>;
+          }
+        }
+    
+        return (
+          <div className="App">
+            <h1>
+              This is a Demo showing several ways to implement Conditional Rendering in React.
+            </h1>
+            {renderAuthButton()}
+            <hr></hr>
+            <button onClick={this.handleClick}>Change LoggedIn Status</button>
+        </div>
+        );
+      }
+    }
